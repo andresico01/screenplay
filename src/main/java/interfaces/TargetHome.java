f8d5c930243917d7592ed6d334249daf1797a225
Andres <adrojas@bancolombia.com.co>
2023-07-11 10:53:07 -0500

    change name

--- src/main/java/interfaces/TargetHome.java
+++ src/main/java/interfaces/TargetHome.java
@@ -1,16 +1,11 @@
 package interfaces;
 
+
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-import javax.xml.validation.Validator;
 
 public class TargetHome {
-
     public final static Target VALIDATION = Target.the("Product check")
             .located(By.xpath("//android.widget.TextView[@text='Products']"));
-
-
-
-
 }
